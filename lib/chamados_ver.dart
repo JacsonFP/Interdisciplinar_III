d5084f2936245a3f92be64bf9d4afbccdc28fe87
Jacson <jacson_ferraz98@hotmail.com>
2019-11-16 10:26:30 -0300

    .

--- lib/chamados_ver.dart
+++ lib/chamados_ver.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:async/async.dart';
 
 class ChamadosVer extends StatefulWidget {
   @override
@@ -10,9 +11,77 @@ class _ChamadosVerState extends State<ChamadosVer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[],
-      ),
-    );
+        body: TabBarView(
+      children: <Widget>[
+        StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection("chamados").snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return new Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else
+                return new ListView(
+                    children: snapshot.data.documents.map((document) {
+                  int status = document["status"];
+                  return new Card(
+                      elevation: 5.0,
+                      margin: EdgeInsets.all(8),
+                      child: ExpansionTile(
+                          title: Text(
+                            "${document["titulo"]}",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[700],
+                            ),
+                          ),
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        "Titulo: ${document["chamados"]["titulo"]}",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(
+                                          "Descrição: ${document["descricao"]}"),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text("Local: ${document["filiais"]}"),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(
+                                          "Categoria: ${document["categoria"]}"),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(
+                                          "Prioridade: ${document["prioridade"]}"),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(
+                                          "Tipo de Serviço: ${document["tipo"]}"),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                    ]))
+                          ]));
+                }).toList());
+            }),
+      ],
+    ));
   }
 }
